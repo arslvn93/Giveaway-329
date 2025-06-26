81d6a2bd47dce8b4ac312d8e4ca12e761aaaebec
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-06-26 14:50:24 -0400

    Update Site

--- config.js
+++ config.js
@@ -168,7 +168,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1750963718817_0",
+      "id": "q_auto_1750963823307_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -190,7 +190,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1750963718817_1",
+      "id": "q_auto_1750963823307_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -212,7 +212,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1750963718817_2",
+      "id": "q_auto_1750963823307_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
@@ -258,9 +258,9 @@ const config = {
       "email": "eric.l.palin@gmail.com"
     },
     "footerDisclaimer": "This giveaway is operated by Eric Palin and is not affiliated with Calgary Stampede or Shania Twain. No purchase needed. Winner chosen at random July 4, 2025. Must be 18+ and Calgary-area resident to enter.",
-    "organizerLogoUrl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.remaxcommercial.com%2Fsingle-office%2F68328%2Fre-max-house-of-real-estate%2F&psig=AOvVaw3NDe98HPWl-nQq_ehVEtgW&ust=1751049639325000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCODa-a_ej44DFQAAAAAdAAAAABAE",
-    "secondaryLogoUrl": "https://papiphotos.remax-im.com/Person/102096352/MainPhoto_cropped/MainPhoto_cropped.jpg",
-    "secondaryLogoAlt": "",
+    "organizerLogoUrl": "https://papiphotos.remax-im.com/Person/102096352/MainPhoto_cropped/MainPhoto_cropped.jpg",
+    "secondaryLogoUrl": "https://img.boldtrail.com/gF4B2QTQJjh5gqJV78odfWm2YmvvFqCbz3fIoGT9JMs/rs:fit:500/plain/https://papiphotos.remax-im.com/Office/100430901/OfficeLogo_cropped/OfficeLogo_cropped.jpg",
+    "secondaryLogoAlt": "Re/Max Real Estate Central Alberta Logo",
     "organizerName": "Eric Palin",
     "brokerageName": "RE/MAX House of Real Estate",
     "address": "4034 16 St SW, Calgary, AB ",
